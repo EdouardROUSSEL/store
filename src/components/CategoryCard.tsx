95a51fc31edde4d3b5bf30e082cef08a9b959c3b
Edouard ROUSSEL <pro.edouard.roussel@gmail.com>
2024-10-07 15:42:14 +0200

    V2

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,5 +1,5 @@
 import { ProductListTypes } from "@/app/types";
-import ProductsPanelGrid from "./ProductsPanelGrid";
+import ProductsPanelGrid from "./ProductsPanelGrid2";
 import Link from "next/link";
 
 export default function CategoryCard({
@@ -16,7 +16,7 @@ export default function CategoryCard({
   addButton?: boolean;
 }) {
   return (
-    <div className="flex md:flex-row flex-col md:space-x-6 md:space-y-0 space-y-10 w-full">
+    <div className="flex md:flex-row flex-col md:space-x-6 md:space-y-0 space-y-10 ">
       <Link
         href={path}
         className="text-white flex flex-col justify-end p-6 aspect-square md:w-1/2 w-full bg-gray-300 rounded-3xl "
